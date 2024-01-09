cbe679d5f9aae57d411d43eb9314087bdd2e3f2f
Huzaifa-A750 <huzaifaafzal750@gmail.com>
2024-01-09 23:22:10 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1 className="text-5xl font-bold underline bg-blue-600 p-24 text-red-700">
+          Hello world!
+       </h1>
+    </div>
+  );
+}
+
+export default App;
