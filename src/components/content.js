87c3923fa69e88efcbc590cd20a73046633392d6
Huzaifa-A750 <huzaifaafzal750@gmail.com>
2024-01-12 02:04:11 +0500

    new

--- src/components/content.js
+++ src/components/content.js
@@ -1,15 +1,23 @@
-import './content.css';
+import "./content.css";
 
 export default function content() {
-  return <div className='bg-gradient mb-20'>
-    <div className='justify-center grid text-center pt-20 pb-10 shadow-slate-400 shadow-md'>
-      <p className='font-[AirBnB-ExtraBold] text-[#3E8180] text-2xl md:text-[42px] font-extrabold leading-9 md:leading-[50px]'>Discover New Construction Homes <br /> Before They Hit The Market</p>
-      <p className='text-[#322C4E] pt-10 font-[AirBnB-Medium] text-md md:text-xl leading-normal'>From move-in-ready to custom homes, fresh<span className='font-[AirBnB-ExtraBold]'>Builds</span> is the <br />  modern way to find + buy a new construction house.</p>
-      <div className='pt-8'>
-        <button className=' rounded-md h-[55px] w-[205px] bg-[#E87F3F] text-white font-[AirBnB-Bold]'>Search freshbuilds</button>
+  return (
+    <div className="bg-gradient mb-20">
+      <div className="justify-center grid text-center pt-20 pb-10 shadow-slate-400 shadow-md">
+        <p className="font-[AirBnB-ExtraBold] text-[#3E8180] text-2xl md:text-[42px] font-extrabold leading-9 md:leading-[50px]">
+          Discover New Construction Homes <br /> Before They Hit The Market
+        </p>
+        <p className="text-[#322C4E] pt-10 font-[AirBnB-Medium] text-md md:text-xl leading-normal">
+          From move-in-ready to custom homes, fresh
+          <span className="font-[AirBnB-ExtraBold]">Builds</span> is the <br />{" "}
+          modern way to find + buy a new construction house.
+        </p>
+        <div className="pt-8">
+          <button className=" rounded-md h-[55px] w-[205px] bg-[#E87F3F] text-white font-[AirBnB-Bold]">
+            Search freshbuilds
+          </button>
+        </div>
       </div>
     </div>
-    
-  </div>;
+  );
 }
-
